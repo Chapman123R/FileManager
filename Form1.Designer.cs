69e931626f857573560feaea462132a17babc408
User <User@DESKTOP-3G79H5S>
2023-10-11 11:56:34 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace FileManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(95, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(393, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(504, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CD_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 66);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(414, 320);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.ItemActivate += new System.EventHandler(this.ItemActive);
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.Click += new System.EventHandler(this.listView1_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "ikonka_papka-2048x1638-1-1536x1229.jpg");
+            this.imageList1.Images.SetKeyName(1, "document-clipart-blank-document-19.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FileManager
+{
+    public partial class Form1 : Form
+    {
+        public int count =0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string name;
+            bool end = true;
+            //do
+            //{
+            //    name = Console.ReadLine();
+
+            //    switch (name)
+            //    {
+            //        case "Help":
+            //            Console.WriteLine("Info -Выводит список файлов в каталоге\nCD-Переход в каталог\nDel-Удаление файла по имени\nDelM-Удаление файла по маске\nCrea-Создание файла\nCopy-Копирование файла");
+            //            break;
+            //        case "Info":
+            //            List<string> q = new List<string>();
+            //            Console.WriteLine("Введите путь каталога");
+            //            string p = Console.ReadLine();
+            //            int s = 0;
+            //            DirectoryInfo dir = new DirectoryInfo(p);
+            //            foreach (var item in dir.GetFiles())
+            //            {
+            //                q.Add(item.Name);
+            //                s++;
+            //            }
+            //            bool flag = true;
+            //            while (flag)
+            //            {
+            //                flag = false;
+            //                for (int k = 0; k < s - 1; ++k)
+            //                    if (q[k].CompareTo(q[k + 1]) > 0)
+            //                    {
+            //                        string buf = q[k];
+            //                        q[k] = q[k + 1];
+            //                        q[k + 1] = buf;
+            //                        flag = true;
+            //                    }
+            //            }
+            //            for (int k = 0; k < s; ++k)
+            //                Console.WriteLine("{0} ", q[k]);
+            //            break;
+
+
+            //        case "CD":
+            //            string filePath = "E:\\Новая папка\\Новая папка\\Новая папка";
+            //            string directoryName;
+            //            int i = 0;
+
+            //            while (filePath != null)
+            //            {
+            //                directoryName = Path.GetDirectoryName(filePath);
+            //                Console.WriteLine("GetDirectoryName('{0}') returns '{1}'",
+            //                    filePath, directoryName);
+            //                filePath = directoryName;
+            //                if (i == 1)
+            //                {
+            //                    filePath = directoryName + @"\";
+            //                }
+            //                i++;
+            //            }
+            //            break;
+
+            //        case "Del":
+            //            string DeleteThis;
+            //            string pyt;
+            //            Console.WriteLine("Введите имя");
+            //            DeleteThis = Console.ReadLine();
+            //            Console.WriteLine("Введите путь");
+            //            pyt = Console.ReadLine();
+            //            string[] Files = Directory.GetFiles(pyt);
+
+            //            foreach (string file in Files)
+            //            {
+            //                if (file.ToUpper().Contains(DeleteThis.ToUpper()))
+            //                {
+            //                    File.Delete(file);
+            //                }
+            //            }
+            //            break;
+
+            //        case "DelM":
+            //            string DeleteThis1;
+            //            string pyt1;
+            //            Console.WriteLine("Введите маску");
+            //            DeleteThis1 = Console.ReadLine();
+            //            Console.WriteLine("Введите путь");
+            //            pyt1 = Console.ReadLine();
+            //            string[] files = Directory.GetFiles(pyt1, DeleteThis1);
+            //            Console.WriteLine("Всего файлов {0}.", files.Length);
+            //            foreach (string f in files)
+            //            {
+            //                Console.WriteLine(f);
+            //                File.Delete(f);
+            //            }
+
+            //            break;
+
+            //        case "Crea":
+            //            string pyt2;
+            //            Console.WriteLine("Введите путь куда создать и имя файла");
+            //            pyt2 = Console.ReadLine();
+            //            File.Create(pyt2);
+            //            break;
+
+            //        case "Copy":
+            //            Console.WriteLine("Введите путь копируемого файла");
+            //            string pathToFile = Console.ReadLine();
+            //            Console.WriteLine("Введите путь куда копировать");
+            //            string pathToFile1 = Console.ReadLine();
+            //            File.Copy(pathToFile, pathToFile1, true);
+            //            break;
+            //        case "Exit":
+            //            end = false;
+            //            break;
+
+            //        default:
+            //            Console.WriteLine("Введена не правильная команда");
+            //            break;
+            //    }
+            //} while (end != false);
+            listView1.SmallImageList = imageList1;
+        }
+
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            if (count == 0)
+            {
+                string path = textBox1.Text;
+                if (path.EndsWith(".exe") || path.EndsWith(".txt") || path.EndsWith(".jpeg") || path.EndsWith(".png") || path.EndsWith(".jpg"))
+                {
+
+                    System.Diagnostics.Process.Start(path);
+                    string text = textBox1.Text;
+                    text = text.Substring(0, text.LastIndexOf(@"\"));
+                    text += @"\";
+                    textBox1.Text = text;
+                    Open_Click(sender, e);
+                }
+                if ( path.EndsWith(@"\")) 
+                {
+                    
+                    // получаем все файлы
+                    string[] files = Directory.GetFiles(path);
+                    string[] folders = Directory.GetDirectories(path);
+                    // перебор полученных файлов
+                    foreach (string file in files)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        // установка названия файла
+                        lvi.Text = file.Remove(0, file.LastIndexOf('\\') + 1);
+                        lvi.ImageIndex = 1; // установка картинки для файла
+                                            // добавляем элемент в ListView
+                        listView1.Items.Add(lvi);
+                    }
+                    foreach (string folder in folders)
+                    {
+                        ListViewItem lvi = new ListViewItem();
+                        // установка названия файла
+                        lvi.Text = folder.Remove(0, folder.LastIndexOf('\\') + 1);
+                        lvi.ImageIndex = 0; // установка картинки для файла
+                                            // добавляем элемент в ListView
+                        listView1.Items.Add(lvi);
+                    }
+                    button1.Enabled = false;
+                }
+                else
+                {
+                    textBox1.Text += @"\";
+                    Open_Click(sender, e);
+                }
+                
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+        private void ItemActive(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems[0].ImageIndex == 0)
+            {
+                textBox1.Text += listView1.SelectedItems[0].Text;
+
+            }
+            if (listView1.SelectedItems[0].ImageIndex == 1)
+            {
+                textBox1.Text += listView1.SelectedItems[0].Text;
+            }
+        }
+
+        private void CD_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            text = text.Substring(0, text.LastIndexOf(@"\"));
+            text = text.Substring(0, text.LastIndexOf(@"\"));
+            text += @"\";
+            textBox1.Text = text;
+            Open_Click(sender, e);
+        }
+    }
+}
